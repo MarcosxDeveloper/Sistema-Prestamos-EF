12ca8982829286dc87037080b1c91aac080f03b8
Marcos Antonio Gonzalez Leon <MarcosxSenpai@Gmail.Com>
2017-07-04 16:10:46 -0400

    Hola

---
+++ CalculadoraDePrestamos/DAL/GeneralContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using CalculadoraDePrestamos.Models;
+
+namespace CalculadoraDePrestamos.DAL
+{
+    public class GeneralContext : DbContext
+    {
+        public GeneralContext() : base("Conexion") { }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+    }
+}
---
+++ CalculadoraDePrestamos/Models/Clientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraDePrestamos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(60)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(60)]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(60)]
+        public string Direccion { get; set; }
+
+        public virtual ICollection<Prestamos> Prestamos { get; set; }
+    }
+}
